ddafea854f44ed8811c9439db7717ba4380cbccd
Tatiana <djembuka@gmail.com>
2019-10-04 12:59:28 +0300

    Masked Input

--- source/components/form.ajax-form/script.js
+++ source/components/form.ajax-form/script.js
@@ -9,29 +9,70 @@
       var $this = $( this ),
           $form = $this.find( 'form' ),
           $response = $this.find( '.b-ajax-form__response' ),
-          $error = $this.find( '.b-ajax-form__error' );
+          $error = $this.find( '.b-ajax-form__error' ),
+          $submitButton = $form.find( '.btn[ type=submit ]' );
+          
+      if ( $.fn.mask ) {
+        $form.find( '#tel' ).mask( '+9 (999) 999 99 99' );
+      }
       
-      $form.submit( function(e) {
-        e.preventDefault();
+      $submitButton.click( function(e) {
+        var errorFlag = false;
+        
+        //validate name input
+        $( 'input[ type="text" ]' ).each( function() {
+          var $textInput = $( this );
+          
+          if ( $.trim( $textInput.val()) === '' ) {
+            $textInput.closest( '.input-field' ).addClass( 'invalid' );
+            errorFlag = true;
+          } else {
+            $textInput.closest( '.input-field' ).removeClass( 'invalid' );
+          }
+          
+        });
+        
+        //validate email input
+        $( 'input[ type="email" ]' ).each( function() {
+          
+          var $emailInput = $( this );
+          var emailString = $emailInput.val();
+          var emailRegExp = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i;
+        
+          if ( !emailString.match( emailRegExp )) {
+            $emailInput.closest( '.input-field' ).addClass( 'invalid' );
+            errorFlag = true;
+          } else {
+            $emailInput.closest( '.input-field' ).removeClass( 'invalid' );
+          }
+          
+        });
         
         //validate tel input
-        var errorFlag = false;
         $( 'input[ type="tel" ]' ).each( function() {
           
           var $telInput = $( this );
           var telString = $telInput.val();
-          var telRegExp = /^[\+]?[0-9]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+          var telRegExp = /^[\+]?[0-9]?\s?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9\s]{4,6}$/im;
         
           if ( !telString.match( telRegExp )) {
-            $telInput.addClass( 'invalid' );
+            $telInput.closest( '.input-field' ).addClass( 'invalid' );
+            $telInput.siblings( 'label' ).addClass( 'active' );
             errorFlag = true;
+          } else {
+            $telInput.closest( '.input-field' ).removeClass( 'invalid' );
           }
           
         });
         
         if ( errorFlag ) {
-          return;
+          e.preventDefault();
+          $form.find( '.form-group.i-invalid:eq(0) input' ).focus();
         }
+      });
+      
+      $form.submit( function(e) {
+        e.preventDefault();
         
         //send ajax
         $.ajax({
