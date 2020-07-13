cc87bf402fd1df912dd98273c1557a6e7b1fc7d8
djembuka <djembuka@gmail.com>
2020-07-13 16:03:15 +0300

    Video banner

--- source/components/video.plyr/script.js
+++ source/components/video.plyr/script.js
@@ -7,21 +7,35 @@
     //local
     var player = new Plyr( '#player', {
       controls: false,
-      autoplay: true
+      autoplay: true,
+      muted: true
     });
 
     player.on( 'canplay', function(e) {
       $( '.b-plyr' ).addClass( 'i-canplay' );
     });
     
-    //youtube
+    //youtube iframe
     var playerYt = new Plyr( '#playerYt', {
       controls: false,
-      autoplay: true
+      autoplay: true,
+      muted: true
     });
 
     playerYt.on( 'ready', function(e) {
       $( '.b-plyr-yt' ).addClass( 'i-canplay' );
+      playerYt.play();
+    });
+    
+    //youtube div
+    var player2 = new Plyr( '#player2', {
+      controls: false,
+      autoplay: true,
+      muted: true
+    });
+    
+    player2.on( 'ready', function(e) {
+      player2.play();
     });
   
     /*if ( window.BX ) {
